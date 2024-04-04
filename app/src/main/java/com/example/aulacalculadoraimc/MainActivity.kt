1fc606c43e0bd6ee80675afefebefe65434c2965
Allyhgf <allyhansel.dev@gmail.com>
2024-04-04 14:05:46 -0300

    Implementado um botão para voltar à inicial na ResultadoActivity
    Implementada toda a lógica de programação das duas telas e seus componentes

--- app/src/main/java/com/example/aulacalculadoraimc/MainActivity.kt
+++ app/src/main/java/com/example/aulacalculadoraimc/MainActivity.kt
@@ -4,20 +4,33 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var buttonCalcular: Button
+    private lateinit var editPeso: EditText
+    private lateinit var editAltura: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         buttonCalcular = findViewById(R.id.button_calcular)
+        editPeso = findViewById(R.id.edit_peso)
+        editAltura = findViewById(R.id.edit_altura)
 
         buttonCalcular.setOnClickListener {
 
             val intent = Intent(this, ResultadoActivity::class.java)
+
+            val peso = editPeso.text.toString()
+            val altura = editAltura.text.toString()
+
+            if ( peso.isNotEmpty() && altura.isNotEmpty() ){
+                intent.putExtra("peso", peso.toDouble())
+                intent.putExtra("altura", altura.toDouble())
+            }
 
             startActivity(intent)
 
--- app/src/main/java/com/example/aulacalculadoraimc/ResultadoActivity.kt
+++ app/src/main/java/com/example/aulacalculadoraimc/ResultadoActivity.kt
@@ -1,11 +1,54 @@
 package com.example.aulacalculadoraimc
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class ResultadoActivity : AppCompatActivity() {
+
+    private lateinit var textPeso: TextView
+    private lateinit var textAltura: TextView
+    private lateinit var textResultado: TextView
+    private lateinit var buttonVoltar: Button
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_resultado)
+
+        textPeso = findViewById(R.id.text_peso)
+        textAltura = findViewById(R.id.text_altura)
+        textResultado = findViewById(R.id.text_resultado)
+
+        val bundle = intent.extras
+        if (bundle != null){
+
+            val peso = bundle.getDouble("peso")
+            val altura = bundle.getDouble("altura")
+
+            textPeso.text = "Peso informado: $peso kg"
+            textAltura.text = "Altura informada: $altura m"
+
+            val imc = peso / (altura * altura)
+
+            val resultado = when {
+                (imc < 18.5) -> "Baixo"
+                (imc in 18.5..24.9) -> "Normal"
+                (imc in 25.0..29.9) -> "Sobrepeso"
+                else ->  "Obeso"
+            }
+
+            textResultado.text = resultado
+
+        }
+
+        buttonVoltar = findViewById(R.id.button_voltar)
+        buttonVoltar.setOnClickListener {
+            finish()
+        }
+
+
     }
 }
