9edae75217b79f42e8ea6e2ff257179594ed8477
Allyhgf <allyhansel.dev@gmail.com>
2024-04-04 13:23:21 -0300

    Configuração do MainActivity, criação do ResultadoActivity.
    Interfaces criadas e redirecionamento para a ResultadoActivy configurado.

--- app/src/main/java/com/example/aulacalculadoraimc/MainActivity.kt
+++ app/src/main/java/com/example/aulacalculadoraimc/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.example.aulacalculadoraimc
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var buttonCalcular: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        buttonCalcular = findViewById(R.id.button_calcular)
+
+        buttonCalcular.setOnClickListener {
+
+            val intent = Intent(this, ResultadoActivity::class.java)
+
+            startActivity(intent)
+
+        }
+
     }
 }
